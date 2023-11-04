b2333616ce926d68104891aee865bdd2a413656b
kahnu044 <djkanha044@gmail.com>
2023-11-04 17:18:49 +0530

    Download PDF Added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
+"use client";
 import Image from 'next/image'
+import dynamic from "next/dynamic";
+
 import { Inter } from 'next/font/google'
+import PDFFile from '../components/PDFFile';
 
 const inter = Inter({ subsets: ['latin'] })
 
-
+const PDFDownloadLink = dynamic(() => import("@react-pdf/renderer").then((mod) => mod.PDFDownloadLink), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
 
 export default function Home() {
   return (
@@ -37,16 +44,9 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-          <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
+        <PDFDownloadLink document={<PDFFile />} filename="FORM">
+          {({ loading }) => (loading ? <button>Loading Document...</button> : <button className="bg-gray-400 px-10 py-4 border rounded-lg font-bold hover:bg-gray-500">Download PDF</button>)}
+        </PDFDownloadLink>
 
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
           <a
